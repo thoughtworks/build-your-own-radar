9455a90685b1e28567f3bb8a303e6aa95e0c7e97
lionescu <lrionescu26@gmail.com>
2016-10-13 14:32:15 +0100

    Medina/Laura: Add legend to radar quadrant
    - only draw home link if it is not present

--- src/site.js
+++ src/site.js
@@ -3,6 +3,7 @@ require('./images/logo.png');
 require('./images/tech-radar-201611-landing-page-wide.png');
 require('./images/tw-logo.png');
 require('./images/favicon.ico');
+require('./images/radar_legend.png');
 
 const GoogleSheetInput = require('./util/factory');
 
