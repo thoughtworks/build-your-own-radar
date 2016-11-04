44066d8c065a07422b6ca6d7927a900982574fe2
Rajeswari Krishnakumar <rajeswak@thoughtworks.com>
2016-11-04 11:20:32 +0530

    Raji/manu: Fix the bug in validating sheet IDs

--- src/util/sheetValidator.js
+++ src/util/sheetValidator.js
@@ -1,16 +1,23 @@
 const SheetNotFoundError = require('../../src/exceptions/sheetNotFoundError');
 const ExceptionMessages = require('./exceptionMessages');
 
-const SheetValidator = function (sheetId) {
+const SheetValidator = function (sheetReference) {
     var self = {};
 
     self.verifySheet = function () {
-        var sheetUrl = '';
-        if (sheetId.trim().startsWith("http")) {
-            sheetUrl = sheetId;
+        var sheetId = '';
+        if (sheetReference.trim().startsWith("http")) {
+            var result = sheetReference.match("d\\/(.*?)\\/pubhtml");
+            if(result === null) {
+                throw new SheetNotFoundError(ExceptionMessages.SHEET_NOT_FOUND);
+            }
+            sheetId = result[1];
+
         } else {
-            sheetUrl = "https://spreadsheets.google.com/feeds/worksheets/" + sheetId + "/public/basic?alt=json";
+            sheetId = sheetReference;
         }
+
+        var sheetUrl = "https://spreadsheets.google.com/feeds/worksheets/" + sheetId + "/public/basic?alt=json";
 
         var xmlhttp = new XMLHttpRequest();
         xmlhttp.open("GET", sheetUrl, false);
