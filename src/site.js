dfd3e3f9adf4f2af9f39ba8e5d1849f4a1bce8bc
Phillip Tann <phillip@tann.uk.com>
2017-01-26 08:12:15 +0000

    -Building your tech radar- to have a solid black banner with white text in the middle of the page

--- src/site.js
+++ src/site.js
@@ -1,6 +1,5 @@
 require('./stylesheets/base.scss');
 require('./images/logo.png');
-require('./images/tech-radar-201611-landing-page-wide.png');
 require('./images/tw-logo.png');
 require('./images/favicon.ico');
 require('./images/radar_legend_v0.1.png');
