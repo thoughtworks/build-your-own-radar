63b23643103e303f5c4e0d81261badec2fd38727
Mohamed Najiullah <mohammed.najiullah@gmail.com>
2017-11-30 11:14:11 +0530

    [Naji/Vivek] Change hero banner and link color

--- src/common.js
+++ src/common.js
@@ -1,4 +1,4 @@
 require('./stylesheets/base.scss');
-require('./images/tech-radar-201711-landing-page-wide.png');
+require('./images/tech-radar-landing-page-wide.png');
 require('./images/tw-logo.png');
 require('./images/favicon.ico');
