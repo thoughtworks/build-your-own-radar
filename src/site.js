39ce14f3c7aabbdf902290de324ba605953d0c01
Saad Yousfi <yousfi.saad@gmail.com>
2018-01-04 11:36:26 +0000

    [WIP] cleaning 1

--- src/site.js
+++ src/site.js
@@ -5,6 +5,13 @@ require('./images/tw-logo.png');
 require('./images/favicon.ico');
 require('./images/radar_legend.png');
 
-const GoogleSheetInput = require('./util/factory');
+var csvContent = require('./radar.csv');
 
-GoogleSheetInput().build();
+var {
+    CSVContent,
+    plotRadar
+} = require('./util/factory')
+
+var blips = CSVContent(csvContent)
+
+plotRadar('title', blips);
