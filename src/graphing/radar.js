861f5af2f69f1e07bfc394ee1612d803cd4af3a3
Saad Yousfi <yousfi.saad@gmail.com>
2018-01-09 17:37:19 +0000

    Add lines/texts

--- src/graphing/radar.js
+++ src/graphing/radar.js
@@ -34,30 +34,38 @@ const Radar = function (size, radar) {
   function toRadian(angleInDegrees) {
     return Math.PI * angleInDegrees / 180;
   }
+  //  :\
+  function plotLine(erase = false) {
+    var startX = size;
+    var endX = size / 2;
 
-  // function plotLines(quadrantGroup, quadrant) {
-  //   var startX = size * (1 - (-Math.sin(toRadian(quadrant.startAngle)) + 1) / 2);
-  //   var endX = size * (1 - (-Math.sin(toRadian(quadrant.startAngle - 90)) + 1) / 2);
+    var startY = size / 2;
+    var endY = 0;
 
-  //   var startY = size * (1 - (Math.cos(toRadian(quadrant.startAngle)) + 1) / 2);
-  //   var endY = size * (1 - (Math.cos(toRadian(quadrant.startAngle - 90)) + 1) / 2);
+    if (startY > endY) {
+      var aux = endY;
+      endY = startY;
+      startY = aux;
+    }
 
-  //   if (startY > endY) {
-  //     var aux = endY;
-  //     endY = startY;
-  //     startY = aux;
-  //   }
+    var svg = d3.select('#radar-plot');
 
-  //   quadrantGroup.append('line')
-  //     .attr('x1', center()).attr('x2', center())
-  //     .attr('y1', startY - 2).attr('y2', endY + 2)
-  //     .attr('stroke-width', 10);
-
-  //   quadrantGroup.append('line')
-  //     .attr('x1', endX).attr('y1', center())
-  //     .attr('x2', startX).attr('y2', center())
-  //     .attr('stroke-width', 10);
-  // }
+    // svg.append('line')
+    //   .attr('x1', center()).attr('x2', center())
+    //   .attr('y1', startY - 2).attr('y2', endY + 2)
+    //   .attr('stroke-width', 10);
+    var line = svg.select('line');
+    if (erase) {
+      if (!line.empty()) {
+        line.remove();
+      }
+    } else if (line.empty()){
+      svg.append('line')
+        .attr('x1', endX).attr('y1', center())
+        .attr('x2', startX).attr('y2', center())
+        .attr('stroke-width', 10);
+    }
+  }
 
   function plotQuadrant(rings, quadrant) {
     var quadrantGroup = svg.append('g')
@@ -82,25 +90,24 @@ const Radar = function (size, radar) {
     return quadrantGroup;
   }
 
-  // function plotTexts(quadrantGroup, rings, quadrant) {
-  //   rings.forEach(function (ring, i) {
-  //     if (quadrant.order === 'first' || quadrant.order === 'fourth') {
-  //       quadrantGroup.append('text')
-  //         .attr('class', 'line-text')
-  //         .attr('y', center() + 4)
-  //         .attr('x', center() + (ringCalculator.getRadius(i) + ringCalculator.getRadius(i + 1)) / 2)
-  //         .attr('text-anchor', 'middle')
-  //         .text(ring.name());
-  //     } else {
-  //       quadrantGroup.append('text')
-  //         .attr('class', 'line-text')
-  //         .attr('y', center() + 4)
-  //         .attr('x', center() - (ringCalculator.getRadius(i) + ringCalculator.getRadius(i + 1)) / 2)
-  //         .attr('text-anchor', 'middle')
-  //         .text(ring.name());
-  //     }
-  //   });
-  // }
+  function plotTexts(erase) {
+    var svg = d3.select('#radar-plot');
+    var texts = svg.selectAll('.line-text');
+    if (erase) {
+      if (! texts.empty()) {
+        texts.remove();
+      }
+    } else if (texts.empty()){
+      ['adopt', 'trial', 'assess', 'hold'].forEach(function (name, i) {
+        svg.append('text')
+          .attr('class', 'line-text')
+          .attr('y', center() + 4)
+          .attr('x', center() + (ringCalculator.getRadius(i) + ringCalculator.getRadius(i + 1)) / 2)
+          .attr('text-anchor', 'middle')
+          .text(name);
+      });
+    }
+  }
 
   function triangle(x, y, order, group) {
     return group.append('path').attr('d', "M412.201,311.406c0.021,0,0.042,0,0.063,0c0.067,0,0.135,0,0.201,0c4.052,0,6.106-0.051,8.168-0.102c2.053-0.051,4.115-0.102,8.176-0.102h0.103c6.976-0.183,10.227-5.306,6.306-11.53c-3.988-6.121-4.97-5.407-8.598-11.224c-1.631-3.008-3.872-4.577-6.179-4.577c-2.276,0-4.613,1.528-6.48,4.699c-3.578,6.077-3.26,6.014-7.306,11.723C402.598,306.067,405.426,311.406,412.201,311.406")
@@ -135,8 +142,8 @@ const Radar = function (size, radar) {
   function calculateBlipCoordinates(chance, minRadius, maxRadius, startAngle, endAngle) {
     const emptyAngle = 4;
 
-    var radius = chance.floating({min: minRadius + blipWidth , max: maxRadius - blipWidth / 2});
-    if(minRadius === 0){
+    var radius = chance.floating({ min: minRadius + blipWidth, max: maxRadius - blipWidth / 2 });
+    if (minRadius === 0) {
       minRadius = maxRadius / 4;
     }
 
@@ -144,7 +151,7 @@ const Radar = function (size, radar) {
       max: startAngle - emptyAngle,
       min: endAngle + emptyAngle
     });
-    
+
     randomAngle = toRadian(randomAngle);
 
     // var adjustX = Math.sin(toRadian(startAngle)) - Math.cos(toRadian(startAngle));
@@ -262,7 +269,7 @@ const Radar = function (size, radar) {
 
           var clickBlip = function () {
             d3.select('.blip-item-description.expanded').node() !== blipItemDescription.node() &&
-            d3.select('.blip-item-description.expanded').classed("expanded", false);
+              d3.select('.blip-item-description.expanded').classed("expanded", false);
             blipItemDescription.classed("expanded", !blipItemDescription.classed("expanded"));
 
             blipItemDescription.on('click', function () {
@@ -276,14 +283,15 @@ const Radar = function (size, radar) {
     });
   }
 
-  function removeHomeLink(){
-    d3.select('.home-link').remove();
+  function removeHomeLink() {
+    d3.select('.home-link').style('visibility', 'hidden');
   }
 
   function createHomeLink(pageElement) {
     if (pageElement.select('.home-link').empty()) {
       pageElement.append('div')
         .html('&#171; Back to Radar home')
+        .style('visibility', 'hidden')
         .classed('home-link', true)
         .classed('selected', true)
         .on('click', redrawFullRadar)
@@ -291,10 +299,13 @@ const Radar = function (size, radar) {
         .attr('fill', '#626F87')
         .append('path')
         .attr('d', 'M27.6904224,13.939279 C27.6904224,13.7179572 27.6039633,13.5456925 27.4314224,13.4230122 L18.9285959,6.85547454 C18.6819796,6.65886965 18.410898,6.65886965 18.115049,6.85547454 L9.90776939,13.4230122 C9.75999592,13.5456925 9.68592041,13.7179572 9.68592041,13.939279 L9.68592041,25.7825947 C9.68592041,25.979501 9.74761224,26.1391059 9.87092041,26.2620876 C9.99415306,26.3851446 10.1419265,26.4467108 10.3145429,26.4467108 L15.1946918,26.4467108 C15.391698,26.4467108 15.5518551,26.3851446 15.6751633,26.2620876 C15.7984714,26.1391059 15.8600878,25.979501 15.8600878,25.7825947 L15.8600878,18.5142424 L21.4794061,18.5142424 L21.4794061,25.7822933 C21.4794061,25.9792749 21.5410224,26.1391059 21.6643306,26.2620876 C21.7876388,26.3851446 21.9477959,26.4467108 22.1448776,26.4467108 L27.024951,26.4467108 C27.2220327,26.4467108 27.3821898,26.3851446 27.505498,26.2620876 C27.6288061,26.1391059 27.6904224,25.9792749 27.6904224,25.7822933 L27.6904224,13.939279 Z M18.4849735,0.0301425662 C21.0234,0.0301425662 23.4202449,0.515814664 25.6755082,1.48753564 C27.9308469,2.45887984 29.8899592,3.77497963 31.5538265,5.43523218 C33.2173918,7.09540937 34.5358755,9.05083299 35.5095796,11.3015031 C36.4829061,13.5518717 36.9699469,15.9439104 36.9699469,18.4774684 C36.9699469,20.1744196 36.748098,21.8101813 36.3044755,23.3844521 C35.860551,24.9584216 35.238498,26.4281731 34.4373347,27.7934053 C33.6362469,29.158336 32.6753041,30.4005112 31.5538265,31.5197047 C30.432349,32.6388982 29.1876388,33.5981853 27.8199224,34.3973401 C26.4519041,35.1968717 24.9791531,35.8176578 23.4016694,36.2606782 C21.8244878,36.7033971 20.1853878,36.9247943 18.4849735,36.9247943 C16.7841816,36.9247943 15.1453837,36.7033971 13.5679755,36.2606782 C11.9904918,35.8176578 10.5180429,35.1968717 9.15002449,34.3973401 C7.78223265,33.5978839 6.53752245,32.6388982 5.41612041,31.5197047 C4.29464286,30.4005112 3.33339796,29.158336 2.53253673,27.7934053 C1.73144898,26.4281731 1.10909388,24.9584216 0.665395918,23.3844521 C0.22184898,21.8101813 0,20.1744196 0,18.4774684 C0,16.7801405 0.22184898,15.1446802 0.665395918,13.5704847 C1.10909388,11.9962138 1.73144898,10.5267637 2.53253673,9.16153157 C3.33339796,7.79652546 4.29464286,6.55435031 5.41612041,5.43523218 C6.53752245,4.3160387 7.78223265,3.35675153 9.15002449,2.55752138 C10.5180429,1.75806517 11.9904918,1.13690224 13.5679755,0.694183299 C15.1453837,0.251464358 16.7841816,0.0301425662 18.4849735,0.0301425662 L18.4849735,0.0301425662 Z');
+    } else {
+      pageElement.select('.home-link')
+        .style('visibility', 'visible');
     }
   }
 
-  function removeRadarLegend(){
+  function removeRadarLegend() {
     d3.select('.legend').remove();
   }
 
@@ -359,10 +370,11 @@ const Radar = function (size, radar) {
   function redrawFullRadar() {
     removeHomeLink();
     removeRadarLegend();
-
+    plotLine(true);
+    plotTexts(true);
     svg.transition()
-        .style('left', 0).style('right', 0)
-        .style('margin-left', null);
+      .style('left', 0).style('right', 0)
+      .style('margin-left', null);
 
     d3.selectAll('.button')
       .classed('selected', false)
@@ -474,21 +486,24 @@ const Radar = function (size, radar) {
     var translateXAll = (1 - adjustX) / 2 * size * scale / 2 + ((1 - adjustX) / 2 * (1 - scale / 2) * size);
     var translateYAll = (1 + adjustY) / 2 * size * scale / 2;
 
-    var moveLeft  = 0;
+    var moveLeft = 0;
 
     var blipScale = 1;
     var blipTranslate = (1 - blipScale) / blipScale;
 
     var rotateAngle = 90 - startAngle;
 
+    plotLine();
+    plotTexts();
+
     svg.transition()
-        .style('left', moveLeft + 'px')
-        .style('margin-left', 0);
+      .style('left', moveLeft + 'px')
+      .style('margin-left', 0);
     d3.select('.quadrant-group-' + order)
       .transition()
       .duration(1000)
       .attr('transform', 'translate(' + translateX + ',' + translateY + ')scale(' + scale + ') rotate(' + (rotateAngle) + ', ' + (center()) + ', ' + (center()) + ')')
-      // .attr('transform', 'rotate(1)');
+    // .attr('transform', 'rotate(1)');
     d3.selectAll('.quadrant-group-' + order + ' .blip-link text').each(function () {
       var x = d3.select(this).attr('x');
       var y = d3.select(this).attr('y');
@@ -509,7 +524,7 @@ const Radar = function (size, radar) {
 
 
 
-    if (d3.select('.legend.legend-' + order).empty()){
+    if (d3.select('.legend.legend-' + order).empty()) {
       // drawLegend(order);
     }
   }
@@ -526,7 +541,10 @@ const Radar = function (size, radar) {
     quadrants = radar.quadrants();
     var header = plotRadarHeader();
 
+
     plotQuadrantButtons(quadrants, header);
+
+    createHomeLink(d3.select('header'));
 
     radarElement.style('height', size + 14 + 'px');
     svg = radarElement.append("svg").call(tip);
@@ -540,6 +558,7 @@ const Radar = function (size, radar) {
     });
 
     plotRadarFooter();
+
   };
 
   return self;
