08eb132aeff40e40eedcb75e3c29d8f437077669
Saad Yousfi <yousfi.saad@gmail.com>
2018-02-09 16:04:38 +0000

    fix deprecated call

--- back/auth.js
+++ back/auth.js
@@ -23,7 +23,7 @@ const basicAuthe = function(username, password) {
 
     if (!user || user.name !== username || user.pass !== password) {
       res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
-      return res.send(401);
+      return res.sendStatus(401);
     }
 
     next();
