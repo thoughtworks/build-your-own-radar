00c729f616b77bc639686dc109c9e6b10dfae76a
Saad Yousfi <yousfi.saad@gmail.com>
2018-02-20 18:10:26 +0000

    Elegance of HOC

--- src/components/src-backoffice/src/components/Header/index.js
+++ src/components/src-backoffice/src/components/Header/index.js
@@ -5,32 +5,31 @@ import { withRouter } from 'react-router-dom';
 
 import logo from '../../SQLI_logo.png';
 
-const WithRouteState = (props) => {
+const MenuItemWithActiveState = withRouter((props) => {
   const pathname = props.location.pathname;
   const to = props.to;
   return <Menu.Item active={to === pathname} {...props} />;
-}
+});
 
 class Header extends Component {
   render() {
-    const location = this.props.location;
 
     return <Menu fixed='top'>
       <Container style={{ fontSize: "130%" }}>
-        <WithRouteState location={location} as={Link} to='/' header style={{ flexDirection: 'row' }}>
+        <MenuItemWithActiveState as={Link} to='/' header style={{ flexDirection: 'row' }}>
           <Image
             size='tiny'
             src={logo}
             style={{ marginRight: '1.5em' }}
           />
           Technologies Radar
-        </WithRouteState>
-        <WithRouteState location={location} as={Link} to='/admin'>Admin</WithRouteState>
-        <WithRouteState location={location} as={Link} to='/feedbacks'>Feedbacks</WithRouteState>
-        <WithRouteState location={location} as={Link} to='/about'>About</WithRouteState>
+        </MenuItemWithActiveState>
+        <MenuItemWithActiveState as={Link} to='/admin'>Admin</MenuItemWithActiveState>
+        <MenuItemWithActiveState as={Link} to='/feedbacks'>Feedbacks</MenuItemWithActiveState>
+        <MenuItemWithActiveState as={Link} to='/about'>About</MenuItemWithActiveState>
       </Container>
     </Menu>
   }
 }
 
-module.exports = withRouter(Header);
+module.exports = Header;
