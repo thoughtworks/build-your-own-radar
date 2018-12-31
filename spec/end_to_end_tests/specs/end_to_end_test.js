8450f16277f9e1b74d1e175ac84f97fe97369a53
Gowtham <gowthamraj198@gmail.com>
2018-12-31 12:31:32 +0530

    Gowtham: Seperate TC for search

--- spec/end_to_end_tests/specs/end_to_end_test.js
+++ spec/end_to_end_tests/specs/end_to_end_test.js
@@ -3,12 +3,12 @@ var byorPage = require('../pageObjects/byor_page')
 var radarPage = require('../pageObjects/radar_page')
 // var config = require('../../../cypress.json')
 
-Cypress.on('uncaught:exception', (err, runnable) => {
-  if (err) {
-    console.log(err.stack)
-  }
-  return false
-})
+// Cypress.on('uncaught:exception', (err, runnable) => {
+//   if (err) {
+//     console.log(err.stack)
+//   }
+//   return false
+// })
 
 describe('Build your radar', function () {
   it('validate 1st sheet', function () {
@@ -26,11 +26,19 @@ describe('Validate multiple sheet', function () {
     cy.visit(Cypress.env('host'))
     byorPage.provideExcelName()
     byorPage.clickSubmitButton()
-    radarPage.searchTheBlip()
-    radarPage.validateBlipSearch()
     radarPage.clickSheet2()
     radarPage.clickTheBlipFromInteractiveSection()
     radarPage.clickTheBlip()
     radarPage.validateBlipDescription('testing')
   })
+})
+
+describe('Validate search', function () {
+    it('validate search', function () {
+        cy.visit(Cypress.env('host'))
+        byorPage.provideExcelName()
+        byorPage.clickSubmitButton()
+        radarPage.searchTheBlip()
+        radarPage.validateBlipSearch()
+    })
 })
