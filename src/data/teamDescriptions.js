fed4f7bdac0232b41f6505f64d94cb5650cbb4f2
JessicaBergsBBC <jessica.bergs@bbc.co.uk>
2019-08-23 10:50:27 +0100

    Add team descriptions panel

---
+++ src/data/teamDescriptions.js
@@ -0,0 +1,7 @@
+const teamDescriptions = {
+  "Education": "[Team description Education]",
+  "World Service Media": "[Team description WSM]",
+  "Archive Services": "[Team description WSM]"
+}
+
+module.exports = teamDescriptions
