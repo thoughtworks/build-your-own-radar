ac96f99d85b96fa071af3ec277c8ac3dc6055f95
Alex.Quintero <alex.quintero@imaginelearning.com>
2020-03-04 17:15:18 -0700

    customize icons

--- src/util/inputSanitizer.js
+++ src/util/inputSanitizer.js
@@ -33,7 +33,7 @@ const InputSanitizer = function () {
     var blip = trimWhiteSpaces(rawBlip)
     blip.description = sanitizeHtml(blip.description, relaxedOptions)
     blip.name = sanitizeHtml(blip.name, restrictedOptions)
-    blip.isNew = sanitizeHtml(blip.isNew, restrictedOptions)
+    blip.icon = sanitizeHtml(blip.icon, restrictedOptions)
     blip.ring = sanitizeHtml(blip.ring, restrictedOptions)
     blip.quadrant = sanitizeHtml(blip.quadrant, restrictedOptions)
 
@@ -45,19 +45,19 @@ const InputSanitizer = function () {
 
     const descriptionIndex = header.indexOf('description')
     const nameIndex = header.indexOf('name')
-    const isNewIndex = header.indexOf('isNew')
+    const iconIndex = header.indexOf('icon')
     const quadrantIndex = header.indexOf('quadrant')
     const ringIndex = header.indexOf('ring')
 
     const description = descriptionIndex === -1 ? '' : blip[descriptionIndex]
     const name = nameIndex === -1 ? '' : blip[nameIndex]
-    const isNew = isNewIndex === -1 ? '' : blip[isNewIndex]
+    const icon = iconIndex === -1 ? '' : blip[iconIndex]
     const ring = ringIndex === -1 ? '' : blip[ringIndex]
     const quadrant = quadrantIndex === -1 ? '' : blip[quadrantIndex]
 
     blip.description = sanitizeHtml(description, relaxedOptions)
     blip.name = sanitizeHtml(name, restrictedOptions)
-    blip.isNew = sanitizeHtml(isNew, restrictedOptions)
+    blip.icon = sanitizeHtml(icon, restrictedOptions)
     blip.ring = sanitizeHtml(ring, restrictedOptions)
     blip.quadrant = sanitizeHtml(quadrant, restrictedOptions)
 
