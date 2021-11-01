ce96c246f56bef1f0a86ae67abffb9d16ba8a85f
Per Gunnar Paulsen <pergpau@gmail.com>
2021-11-01 11:23:49 +0100

    Update title + temp comment out loading page

--- src/site.js
+++ src/site.js
@@ -2,7 +2,12 @@ require('./common')
 require('./images/logo.png')
 require('./images/radar_legend.png')
 require('./gtm.js')
+const { config } = require('./config.js')
 
 const GoogleSheetInput = require('./util/factory')
 
 GoogleSheetInput().build()
+
+if (!window.location.href.includes('?sheetId')) {
+    window.location.replace(window.location.href + '?sheetId=' + config.sheetUrl)
+}
