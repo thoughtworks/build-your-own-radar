0c2a522c37c616c8caea4a4005d7158dd7b97c4c
Per Gunnar Paulsen <pergpau@gmail.com>
2021-11-02 10:31:04 +0100

    Proper sheet url

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const config = {
-    sheetUrl: "https%3A%2F%2Fraw.githubusercontent.com%2Foslokommune%2Forigo-tech-radar%2Fmaster%2Fdata%2Fradar-dummy-data.csv"
+    sheetUrl: "https://raw.githubusercontent.com/oslokommune/origo-tech-radar/master/data/radar-dummy-data.csv"
 }
