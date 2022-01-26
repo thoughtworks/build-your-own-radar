ba190a89d2f364b238bc0ddb929d628232738ce4
Igwe Kalu <igwe.kalu@thoughtworks.com>
2022-01-26 10:36:03 +0100

    FIX remove deprecated file-loader
    - Added "@babel/preset-env" package as appears to be customary

--- webpack.config.js
+++ webpack.config.js
@@ -75,7 +75,14 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: [{ loader: 'babel-loader' }],
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        ],
       },
       {
         test: /\.scss$/,
