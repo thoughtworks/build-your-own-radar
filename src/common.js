f85e77efb6b65f9ad6d6f9b4d5f552ec09d9d00a
Jeroen Benckhuijsen <jeroen.benckhuijsen@group9.nl>
2022-06-17 13:29:32 +0200

    Dynamically load the logo

--- src/common.js
+++ src/common.js
@@ -1,7 +1,9 @@
 require('./stylesheets/base.scss')
 require('./images/tech-radar-landing-page-wide.png')
-require('./images/tw-logo.png')
 require('./images/favicon.ico')
 require('./images/search-logo-2x.svg')
 require('./images/banner-image-mobile.jpg')
 require('./images/banner-image-desktop.jpg')
+
+const config = require('config')
+require('.' + config.tenant.logo.image)
