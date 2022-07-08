5efecb685dd6e072aef9e7aac16d23972d15846c
Devansh Sharma <devansh.sharma@thoughtworks.com>
2022-07-08 19:39:55 +0530

    [Devansh/Saranya] Fix docker push script to avoid latest tag overwrite

--- docker_push.sh
+++ docker_push.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
 docker login -u $DOCKER_USER -p $DOCKER_PASS
+
 export REPO=wwwthoughtworks/build-your-own-radar
-export TAG=latest
-export COMMIT=${CIRCLE_SHA1:0:8}
-docker build -f Dockerfile -t $REPO:$COMMIT .
-docker tag $REPO:$COMMIT $REPO:$TAG
-docker push $REPO
+export TAG=${CIRCLE_SHA1:0:8}
+
+docker build -f Dockerfile -t $REPO:latest .
+docker tag $REPO:latest $REPO:$TAG
+
+docker push $REPO:latest
+docker push $REPO:$TAG
