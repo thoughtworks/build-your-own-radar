8302d7df6e385c83c9de60f560c024757357c372
Devansh Sharma <devansh.sharma@thoughtworks.com>
2023-02-22 16:28:30 +0530

    [Devansh] #145581 Add sass-loader webpack config for prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,9 @@ const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
 const path = require('path')
 const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const postcssPresetEnv = require('postcss-preset-env')
+const cssnano = require('cssnano')
 const main = ['./src/site.js']
 const fs = require('fs')
 const config = require('./src/config')
@@ -15,6 +18,41 @@ module.exports = merge(common, {
   entry: { main },
   performance: {
     hints: false,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: { importLoaders: 1, modules: 'global', url: false },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  postcssPresetEnv({ browsers: 'last 2 versions' }),
+                  cssnano({
+                    preset: ['default', { discardComments: { removeAll: true } }],
+                  }),
+                ],
+              },
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              additionalData: scssVariables,
+            },
+          },
+        ],
+      },
+    ],
   },
   plugins: [
     new webpack.NoEmitOnErrorsPlugin(),
