b5a07221fecd3e7cdb4c29cb6b7e554c8066c0f1
Devansh Sharma <devansh.sharma@thoughtworks.com>
2023-02-23 16:23:00 +0530

    [Devansh/Sudhin] Disable cypress video recording

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,6 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   chromeWebSecurity: false,
   screenshotsFolder: 'spec/end_to_end_tests/reports/screenshots',
-  videosFolder: 'spec/end_to_end_tests/reports/videos',
   reporter: 'mochawesome',
   reporterOptions: {
     reportDir: 'spec/end_to_end_tests/reports',
@@ -20,4 +19,5 @@ module.exports = defineConfig({
     specPattern: 'spec/end_to_end_tests/specs/**/*.cy.{js,jsx,ts,tsx}',
     supportFile: false,
   },
+  video: false
 })
