a99ff7a91ae0f793174bd4fb499e4571a1ee1d6d
Adam Setch <adam.setch@outlook.com>
2023-07-21 18:33:29 -0400

    fix: correct case for function name (#332)

--- src/graphing/blips.js
+++ src/graphing/blips.js
@@ -203,7 +203,7 @@ function getGroupBlipTooltipText(ringBlips) {
   return tooltipText
 }
 
-const findexistingBlipCoords = function (ringIndex, deg) {
+const findExistingBlipCoords = function (ringIndex, deg) {
   const blipWidth = graphConfig.existingGroupBlipWidth
   const ringWidth = getRingRadius(ringIndex) - getRingRadius(ringIndex - 1)
   const halfRingRadius = getRingRadius(ringIndex) - ringWidth / 2
@@ -220,7 +220,7 @@ function findNewBlipCoords(existingCoords) {
 }
 
 const groupBlipsBaseCoords = function (ringIndex) {
-  const existingCoords = findexistingBlipCoords(ringIndex + 1, graphConfig.groupBlipAngles[ringIndex])
+  const existingCoords = findExistingBlipCoords(ringIndex + 1, graphConfig.groupBlipAngles[ringIndex])
 
   return {
     existing: existingCoords,
