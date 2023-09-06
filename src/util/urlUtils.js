6a9088ee42552d24b99df18f933a5a51e1c3b216
Devansh Sharma <devansh.sharma@thoughtworks.com>
2023-09-06 16:17:39 +0530

    [Devansh] Fix alternative sheets behvaiour for sheetId query param

--- src/util/urlUtils.js
+++ src/util/urlUtils.js
@@ -1,13 +1,14 @@
 const QueryParams = require('../util/queryParamProcessor')
-const config = require('../config')
+
 function constructSheetUrl(sheetName) {
   const noParamsUrl = window.location.href.substring(0, window.location.href.indexOf(window.location.search))
   const queryParams = QueryParams(window.location.search.substring(1))
   const sheetUrl =
     noParamsUrl +
-    (config().featureToggles.UIRefresh2022
-      ? '?documentId=' + queryParams.documentId
-      : '?sheetId=' + queryParams.sheetId) +
+    '?' +
+    ((queryParams.documentId && `documentId=${encodeURIComponent(queryParams.documentId)}`) ||
+      (queryParams.sheetId && `sheetId=${encodeURIComponent(queryParams.sheetId)}`) ||
+      '') +
     '&sheetName=' +
     encodeURIComponent(sheetName)
   return sheetUrl
