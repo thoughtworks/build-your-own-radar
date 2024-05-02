2b2fd1b8d235c9eb6d0858ca6f1d4c4dc261e0ca
Yashwanth Anandan <yashanandan@gmail.com>
2024-05-02 13:15:41 +0530

    [Yash] Fix anchor tag allowed attributes in InputSanitizer

--- src/util/inputSanitizer.js
+++ src/util/inputSanitizer.js
@@ -7,7 +7,7 @@ const InputSanitizer = function () {
   var relaxedOptions = {
     allowedTags: ['b', 'i', 'em', 'strong', 'a', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'li', 'ul', 'br', 'p', 'u'],
     allowedAttributes: {
-      a: ['href'],
+      a: ['href', 'target', 'rel'],
     },
   }
 
