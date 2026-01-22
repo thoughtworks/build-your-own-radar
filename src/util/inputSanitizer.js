6703a92787e4b900d627ebff91d4fa50b3a02576
Bryan Ramos <bramos@redhat.com>
2026-01-21 19:05:13 -0500

    feat(sanitizer): allow <code> and <pre>
    Allow `<code>` and `<pre>` tags so users can render markdown code blocks.
    
    Signed-off-by: Bryan Ramos <bramos@redhat.com>

--- src/util/inputSanitizer.js
+++ src/util/inputSanitizer.js
@@ -5,9 +5,10 @@ const _ = {
 
 const InputSanitizer = function () {
   var relaxedOptions = {
-    allowedTags: ['b', 'i', 'em', 'strong', 'a', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'li', 'ul', 'br', 'p', 'u'],
+    allowedTags: ['b', 'i', 'em', 'strong', 'a', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'li', 'ul', 'br', 'p', 'u', 'code', 'pre'],
     allowedAttributes: {
       a: ['href', 'target', 'rel'],
+      code: ['classes'],
     },
   }
 
